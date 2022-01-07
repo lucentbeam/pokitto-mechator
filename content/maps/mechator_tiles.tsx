354608c4e64408ba27fcf9d29246526649595f80
Ben (Mint) <bhanken@netzero.net>
2022-01-07 07:58:49 -0700

    Finish island rough cut

---
+++ content/maps/mechator_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="mechator" tilewidth="6" tileheight="6" tilecount="252" columns="18">
+ <image source="../art/exports/jungletiles.png" width="108" height="84"/>
+</tileset>
