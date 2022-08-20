37069783a031876f5806290705398befaa31380d
Benjamin <bhanken@netzero.net>
2022-08-20 11:55:31 -0500

    Fix bug in sky map RLE rendering

--- game/rendering/skytilemap.cpp
+++ game/rendering/skytilemap.cpp
@@ -12,17 +12,17 @@ SkyTilemap::SkyTilemap(const uint8_t tiles[][18*18+2], const uint8_t *map, const
 
 void SkyTilemap::draw()
 {
-    int16_t x = std::floor(Camera::draw_x()/18.0f) - 1;
-    int16_t y = std::floor(Camera::draw_y()/18.0f) - 1;
+    int16_t x = std::floor(Camera::draw_x()/18.0f);
+    int16_t y = std::floor(Camera::draw_y()/18.0f);
     int sx = int(x) * 18 - Camera::draw_x();
     int sy = int(y) * 18 - Camera::draw_y();
-    uint16_t x_upper = x + render_width + 1;
+    int16_t x_upper = x + render_width;
     if (x_upper > m_mapwidth) x_upper = m_mapwidth;
-    uint16_t y_upper = y + render_height + 1;
+    int16_t y_upper =y +render_height;
     if (y_upper > m_mapheight) y_upper = m_mapheight;
-    uint16_t x_lower = x < 0 ? 0 : x;
+    int16_t x_lower = x < 0 ? 0 : x;
     if (x_lower >= x_upper) return;
-    uint16_t y_lower = y < 0 ? 0 : y;
+    int16_t y_lower = y < 0 ? 0 : y;
     if (y_lower >= y_upper) return;
     int current_tile;
     for(int16_t j = y_lower; j < y_upper; j++) {
@@ -36,10 +36,11 @@ void SkyTilemap::draw()
                 counter = *(tile + 1);
             }
         }
+        current_tile = *tile;
         for(int16_t i = x_lower; i < x_upper; ++i) {
             if (current_tile != uint8_t(-1)) {
-                RenderSystem::drawShadow(sx + (i-x) * 18, sy + (j-y) * 18 + 3, m_tiles[*tile], m_tiles[*tile][2]);
-                RenderSystem::sprite(sx + (i-x) * 18, sy + (j-y) * 18, m_tiles[*tile], m_tiles[*tile][2]);
+                RenderSystem::drawShadow(sx + (i-x) * 18, sy + (j-y) * 18 + 3, m_tiles[current_tile], m_tiles[current_tile][2]);
+                RenderSystem::sprite(sx + (i-x) * 18, sy + (j-y) * 18, m_tiles[current_tile], m_tiles[current_tile][2]);
             }
             counter--;
             if (counter <= 0) {
